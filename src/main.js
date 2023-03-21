4531bd4f3223110c520f40e7a117e6525ea902b8
Jduchman <jorge.duchman.27@unsch.edu.pe>
2023-03-21 14:50:59 -0500

    diseño completado

---
+++ src/main.js
@@ -0,0 +1,39 @@
+const {app, BrowserWindow, Menu} = require('electron')
+
+let ventana;
+
+let menu = new Menu()
+
+function crearVentana(){
+    ventana = new BrowserWindow({
+        height: 560,
+        width: 950,
+        resizable: false,
+        icon: "./resources/icono.ico",
+        title:"Memorama",
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+    ventana.loadFile('src/pages/home/home.html')
+    ventana.setMenu(menu);
+}
+
+require('electron-reload')(__dirname);
+
+app.whenReady().then(()=>{
+
+    crearVentana();
+
+    app.on('activate', ()=>{
+        if (BrowserWindow.getAllWindows().length === 0) crearVentana();
+    })
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+});
+
+module.exports = {
+    crearVentana
+}
